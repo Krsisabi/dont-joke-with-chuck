6993d6f0516feeb7a4922de8692f3322df035780
Tim Yusupov <timboyus@gmail.com>
2023-08-31 20:04:47 +0300

    init setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+/// <reference types="vite/client" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [svgr(), react(), tsconfigPaths(), pluginRewriteAll()],
+  resolve: {
+    alias: {
+      '~styles': path.resolve(__dirname, 'src/ui/styles'),
+    },
+  },
+});
